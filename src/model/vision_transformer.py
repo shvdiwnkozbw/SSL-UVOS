62a8c0b8efd3120282b0c748dc41d3b06ef53b27
root <qrui9911@outlook.com>
2023-12-04 23:06:21 +0800

    update

--- src/model/vision_transformer.py
+++ src/model/vision_transformer.py
@@ -93,14 +93,12 @@ class Mlp(nn.Module):
         self.act = act_layer()
         self.fc2 = nn.Linear(hidden_features, out_features)
         self.drop = nn.Dropout(drop)
-        # self.fc1_lora = LoRALinear(in_features, hidden_features, False)
-        # self.fc2_lora = LoRALinear(hidden_features, out_features, False)
 
     def forward(self, x):
-        x = self.fc1(x) #+ self.fc1_lora(x)
+        x = self.fc1(x)
         x = self.act(x)
         x = self.drop(x)
-        x = self.fc2(x) #+ self.fc2_lora(x)
+        x = self.fc2(x)
         x = self.drop(x)
         return x
 
@@ -116,25 +114,18 @@ class Attention(nn.Module):
         self.attn_drop = nn.Dropout(attn_drop)
         self.proj = nn.Linear(dim, dim)
         self.proj_drop = nn.Dropout(proj_drop)
-        # self.q_lora = LoRALinear(dim, dim, False)
-        # self.k_lora = LoRALinear(dim, dim, False)
-        # self.v_lora = LoRALinear(dim, dim, False)
-        # self.proj_lora = LoRALinear(dim, dim, False)
 
     def forward(self, x):
         B, N, C = x.shape
         qkv = self.qkv(x).reshape(B, N, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
         q, k, v = qkv[0], qkv[1], qkv[2]
-        # q = q + self.q_lora(x).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
-        # k = k + self.k_lora(x).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
-        # v = v + self.v_lora(x).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
 
         attn = (q @ k.transpose(-2, -1)) * self.scale
         attn = attn.softmax(dim=-1)
         attn = self.attn_drop(attn)
 
         x = (attn @ v).transpose(1, 2).reshape(B, N, C)
-        x = self.proj(x) #+ self.proj_lora(x)
+        x = self.proj(x)
         x = self.proj_drop(x)
         return x, attn, k
 
@@ -300,106 +291,6 @@ class VisionTransformer(nn.Module):
         k = k[:, 1:].view(k.shape[0], h, w, k.shape[-1]*k.shape[-2])
         k = k.permute(0, 3, 1, 2)        
         return output, cls_token, k
-
-
-class VisionTransformerNoCls(nn.Module):
-    """ Vision Transformer """
-    def __init__(self, img_size=[224], patch_size=16, in_chans=3, num_classes=0, embed_dim=768, depth=12,
-                 num_heads=12, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop_rate=0., attn_drop_rate=0.,
-                 drop_path_rate=0.1, norm_layer=nn.LayerNorm, **kwargs):
-        super().__init__()
-        self.num_features = self.embed_dim = embed_dim
-
-        self.patch_embed = PatchEmbed(
-            img_size=img_size[0], patch_size=patch_size, in_chans=in_chans, embed_dim=embed_dim)
-        num_patches = self.patch_embed.num_patches
-
-        self.pos_embed = nn.Parameter(torch.zeros(1, num_patches, embed_dim))
-        self.pos_drop = nn.Dropout(p=drop_rate)
-
-        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, depth)]  # stochastic depth decay rule
-        self.blocks = nn.ModuleList([
-            Block(
-                dim=embed_dim, num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
-                drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i], norm_layer=norm_layer)
-            for i in range(depth)])
-        self.norm = norm_layer(embed_dim)
-
-        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-
-        # Classifier head
-        self.head = nn.Linear(embed_dim, num_classes) if num_classes > 0 else nn.Identity()
-
-        trunc_normal_(self.pos_embed, std=.02)
-        self.apply(self._init_weights)
-
-    def _init_weights(self, m):
-        if isinstance(m, nn.Linear):
-            trunc_normal_(m.weight, std=.02)
-            if isinstance(m, nn.Linear) and m.bias is not None:
-                nn.init.constant_(m.bias, 0)
-        elif isinstance(m, nn.LayerNorm):
-            nn.init.constant_(m.bias, 0)
-            nn.init.constant_(m.weight, 1.0)
-
-    def interpolate_pos_encoding(self, x, w, h):
-        npatch = x.shape[1]
-        N = self.pos_embed.shape[1]
-        if npatch == N and w == h:
-            return self.pos_embed
-        patch_pos_embed = self.pos_embed
-        dim = x.shape[-1]
-        w0 = w // self.patch_embed.patch_size
-        h0 = h // self.patch_embed.patch_size
-        # we add a small number to avoid floating point error in the interpolation
-        # see discussion at https://github.com/facebookresearch/dino/issues/8
-        w0, h0 = w0 + 0.1, h0 + 0.1
-        patch_pos_embed = nn.functional.interpolate(
-            patch_pos_embed.reshape(1, int(math.sqrt(N)), int(math.sqrt(N)), dim).permute(0, 3, 1, 2),
-            scale_factor=(w0 / math.sqrt(N), h0 / math.sqrt(N)),
-            mode='bicubic',
-        )
-        assert int(w0) == patch_pos_embed.shape[-2] and int(h0) == patch_pos_embed.shape[-1]
-        patch_pos_embed = patch_pos_embed.permute(0, 2, 3, 1).view(1, -1, dim)
-        return patch_pos_embed
-
-    def prepare_tokens(self, x):
-        B, nc, h, w = x.shape
-        x, ph, pw = self.patch_embed(x)  # patch linear embedding
-
-        # add positional encoding to each token
-        x = x + self.interpolate_pos_encoding(x, h, w)
-
-        return self.pos_drop(x), ph, pw
-
-    def forward(self, x):
-        x, h, w = self.prepare_tokens(x)
-        for blk in self.blocks:
-            x = blk(x)
-        x = self.norm(x)
-        x = x.view(x.shape[0], h, w, x.shape[-1])
-        x = x.permute(0, 3, 1, 2)
-        avg = self.avgpool(x).flatten(1)
-        return x, avg
-
-    def get_last_selfattention(self, x):
-        x, h, w = self.prepare_tokens(x)
-        for i, blk in enumerate(self.blocks):
-            if i < len(self.blocks) - 1:
-                x = blk(x)
-            else:
-                # return attention of the last block
-                return blk(x, return_attention=True)
-
-    def get_intermediate_layers(self, x, n=1):
-        x = self.prepare_tokens(x)
-        # we return the output tokens from the `n` last blocks
-        output = []
-        for i, blk in enumerate(self.blocks):
-            x = blk(x)
-            if len(self.blocks) - i <= n:
-                output.append(self.norm(x))
-        return output
 
 
 def vit_tiny(patch_size=16, **kwargs):
