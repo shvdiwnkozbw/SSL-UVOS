a28482a35e0655fe1ff752cb01f01da632be6e8d
root <qrui9911@outlook.com>
2023-12-04 14:01:09 +0800

    update memory efficient inference

--- src/eval_oneshot.py
+++ src/eval_oneshot.py
@@ -82,6 +82,70 @@ def hierarchical_cluster(attn, tau, num_iter, device):
         final_mask[i, nms_set==i] = 1
     return final_mask
 
+def mem_efficient_hierarchical_cluster(q, k, scale, tau, num_iter, device):
+    # q t head hw c
+    # k tk head hw c
+    ts = 3
+    bs = 10000
+    final_set = []
+
+    ## use a temporal window for clustering of the first hierarchy to speed up clustering process
+    for t in range(0, q.shape[0], ts):
+        attn = torch.einsum('qhnc,khmc->qkhnm', q[t:t+ts], k) * scale
+        attn = einops.rearrange(attn, 'q k h n m -> (q n) h (k m)')
+        attn = attn.softmax(dim=-1)
+        sample = attn.mean(dim=1).to(device) # thw khw
+        distance = kl_distance(sample, sample)
+        keep_set = []
+        for i in range(0, distance.shape[0], bs):
+            indices = (distance[i:i+bs] <= tau) # bs hw
+            dist = torch.einsum('bn,nc->bc', indices.float(), sample) # bs hw
+            dist = dist / dist.sum(dim=1, keepdim=True)
+            keep_set.append(dist)
+        keep_set = torch.cat(keep_set, dim=0)
+        distance = kl_distance(keep_set, keep_set)
+        indicator = torch.ones(len(keep_set)).to(device)
+        for i in range(len(keep_set)):
+            if indicator[i] == 0:
+                continue
+            indices = (distance[i] <= tau) * (indicator > 0) # K
+            dist = torch.mean(keep_set[indices], dim=0)
+            final_set.append(dist)
+            indicator[indices] = 0
+    keep_set = final_set
+
+    ## clustering on all frames at the following hierarchies until no change
+    for t in range(num_iter):
+        final_set = []
+        keep_set = torch.stack(keep_set, dim=0) # K hw
+        distance = kl_distance(keep_set, keep_set)
+        indicator = torch.ones(len(keep_set)).to(device) # K
+        for i in range(len(keep_set)):
+            if indicator[i] == 0:
+                continue
+            indices = (distance[i] <= tau) * (indicator > 0) # K
+            dist = torch.mean(keep_set[indices], dim=0)
+            final_set.append(dist)
+            indicator[indices] = 0
+        if len(final_set) == len(keep_set):
+            break
+        keep_set = final_set
+    final_set = torch.stack(final_set, dim=0)
+
+    ## calculate cluster assignments as object segmentation masks
+    final_mask = torch.zeros(final_set.shape[0], q.shape[0]*q.shape[2]).to(q.device)
+    for t in range(0, q.shape[0], ts):
+        attn = torch.einsum('qhnc,khmc->qkhnm', q[t:t+ts], k) * scale
+        attn = einops.rearrange(attn, 'q k h n m -> (q n) h (k m)')
+        attn = attn.softmax(dim=-1)
+        sample = attn.mean(dim=1).to(device) # thw khw
+        distance = kl_distance(final_set.to(sample.device), sample) # K thw
+        nms_set = torch.argmin(distance, dim=0)
+        for i in range(final_mask.shape[0]):
+            final_mask[:, t*q.shape[2]:(t+ts)*q.shape[2]][i, nms_set==i] = 1
+    print('cluster centroids:', final_set.shape)           
+    return final_mask
+
 def inference(masks_collection, rgbs, gts, model, T, ratio, tau, device):
     bs = 1
     feats = []
@@ -118,6 +182,39 @@ def inference(masks_collection, rgbs, gts, model, T, ratio, tau, device):
         masks_collection[i].append(mask[i])
     return masks_collection
 
+def mem_efficient_inference(masks_collection, rgbs, gts, model, T, ratio, tau, device):
+    bs = 1
+    feats = []
+    ## extract frame-wise dino features
+    for i in range(0, T, bs):
+        input = rgbs[:, i:i+bs]
+        input = einops.rearrange(input, 'b t c h w -> (b t) c h w')
+        with torch.no_grad():
+            _, _, _, feat = model.encoder(input)
+            feats.append(feat.cpu())
+    feats = torch.cat(feats, 0).to(device) # t c h w
+    print('spatio-temporal feature:', feats.shape)
+
+    ## calculate the spatio-temporal attention, use sparse sampling on keys to reduce computational cost
+    T, C, H, W = feats.shape
+    num_heads = model.temporal_transformer[0].attn.num_heads
+    feats = einops.rearrange(feats, 't c h w -> t (h w) c')
+    feats = model.temporal_transformer[0].norm1(feats) # t hw c
+    qkv = model.temporal_transformer[0].attn.qkv(feats)
+    qkv = qkv.reshape(T, H*W, 3, num_heads, C // num_heads).permute(2, 0, 3, 1, 4) # 3 t h hw c
+    q, k, _ = qkv.cpu().unbind(0) # t h hw c
+    key_indices = torch.arange(T//ratio) * ratio # sparse sampling the keys with a sparsity ratio
+    k = k[key_indices]
+    scale = model.temporal_transformer[0].attn.scale
+
+    ## clustering on the spatio-temporal attention maps and produce segmentation for the whole video
+    dist = mem_efficient_hierarchical_cluster(q, k, scale, tau=tau, num_iter=10000, device=device)
+    dist = einops.rearrange(dist, '(s p) (t h w) -> t s p h w', t=T, p=1, h=H)
+    mask = dist.unsqueeze(1)
+    for i in range(T):
+        masks_collection[i].append(mask[i])
+    return masks_collection
+
 def eval(val_loader, model, device, ratio, tau, save_path=None, writer=None, train=False):
     with torch.no_grad():
         t = time.time()
@@ -140,7 +237,7 @@ def eval(val_loader, model, device, ratio, tau, save_path=None, writer=None, tra
             masks_collection = {}
             for i in range(T):
                 masks_collection[i] = []
-            masks_collection = inference(masks_collection, rgbs, gts, model, T, ratio, tau, device)
+            masks_collection = mem_efficient_inference(masks_collection, rgbs, gts, model, T, ratio, tau, device)
             torch.save(masks_collection, save_path+'/%s.pth' % category[0])
 
 def main(args):
