99795fc8956c79d3982f756c5b295b56b902be17
Mark Ding <41252110+Mark12Ding@users.noreply.github.com>
2024-07-08 13:44:39 +0800

    Update start_rgb.sh

--- start_rgb.sh
+++ start_rgb.sh
@@ -1,5 +1,5 @@
 python -W ignore -m torch.distributed.launch --nproc_per_node=8 --use_env \
-		src/train_RGB_cluster.py \
+	src/train_RGB_cluster.py \
         --basepath /path/to/data \
         --batch_size 1 \
         --seed 0 \
@@ -9,4 +9,4 @@ python -W ignore -m torch.distributed.launch --nproc_per_node=8 --use_env \
         --num_frames 3 \
         --output_path test_log \
         --dino_path dino_small_8.pth \
-		--dataset DAVIS
+	--dataset DAVIS
